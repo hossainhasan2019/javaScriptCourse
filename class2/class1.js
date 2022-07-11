e81410c53e290b07ff524d485bc65dd84d1125af
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-11 16:08:02 +0600

    variables
    class 1 variables types

---
+++ class2/class1.js
@@ -0,0 +1,64 @@
+// variables
+//4 Ways to Declare a JavaScript Variable:
+//Using var name = ...
+//Using let name = ...
+//Using const name = ...
+//Using nothing
+
+var name = `global veriable`
+
+var name = `john`;
+console.log(name);
+
+
+// variable naming convention
+
+// Variable name should be meaningful
+
+//not usage this type variable name
+//var x ='john';
+// y = '47';
+// z = 'male';
+
+
+//usage this type variable name
+// var name = `john`;
+// var age = 47 ;
+// gender = `male`;
+
+//Don't usage any reserved keywords
+// var var =1;
+// var if = 2;
+// var else = 3;
+
+
+// cannot start with nambers 
+
+// var 1name = `john`;
+// var 1age = 47;
+// var 1 gender = `male`;
+
+// cannot contain a space or hyphen.
+// var current user age = 73;
+// var current-user-age = 47;
+
+// camel case (we'll use this one in this course )
+// currentUserAge
+
+// Pascel Case 
+// CurrentUserAge
+
+// snake Case
+// current_user_age
+
+// we usage camel case javascript
+
+
+
+
+
+
+
+
+
+
