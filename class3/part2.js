5770c6828df77861c458c3b9903be93d5c8e6516
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-19 22:42:23 +0600

    JavaScript Assignment Operators
    JavaScript Assignment Operators

---
+++ class3/part2.js
@@ -0,0 +1,33 @@
+/*  JavaScript Assignment Operators
+//  Assignment operators assign values to JavaScript variables.
+//  
+//  Operator	Example	    Same As
+//  =	        x = y	    x = y
+//  +=	        x += y	    x = x + y
+//  -=	        x -= y	    x = x - y
+//  *=	        x *= y	    x = x * y
+//  /=	        x /= y	    x = x / y
+//  %=	        x %= y	    x = x % y
+//  **=	        x **= y	    x = x ** y
+*/
+
+let number1 = 10 ; // ecual sign dia assign kora 
+let number2 = number1 ;
+console.log(number1 , number2);
+
+// assing and arithmetic oprator usage normal ways
+number1 = number1 + 5;
+number1 = number1 * 5;
+
+// assing and arithmetic oprator usage short cut ways
+
+number1 += 5
+number1 *= 5
+
+//+1 er jonno incriment oprator usage 
+number1 += 1; // assing and arithmetic oprator usage short cut ways 
+number1 ++;//+1 er jonno incriment oprators
+
+//-1 er jonno dicriment oprators
+
+number1--; //number1 k 1 mainus kore assing kore dibe
