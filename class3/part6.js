5a0d521fcc999c4523f632419fd08d4853446e92
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-19 22:45:14 +0600

    problem : swap to numbers
    problem : swap to numbers

---
+++ class3/part6.js
@@ -0,0 +1,13 @@
+// problem : swap to numbers
+// a = 10 , b = 10
+//after swapping 
+// a = 20 , b = 10 
+
+let a = 10;
+let b = 20;
+
+let temp = a; // temp = 10
+a = b; // a = 20 , b = 20
+b = temp //a = 20 , b = 20 ;
+
+console.log(a, b);
