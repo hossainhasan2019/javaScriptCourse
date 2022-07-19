172a18a44dfc56184cacf1852a8c4c03859295ee
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-19 22:41:08 +0600

    JavaScript Arithmetic Operators
    JavaScript Arithmetic Operators

---
+++ class3/part1.js
@@ -0,0 +1,25 @@
+//  JavaScript Arithmetic Operators
+//  Arithmetic operators are used to perform arithmetic on numbers:
+
+//  Operator	Description
+//  +	Addition
+//  -	Subtraction
+//  *	Multiplication
+//  **	Exponentiation (ES2016)
+//  /	Division
+//  %	Modulus (Division Remainder)
+//  ++	Increment
+//  --	Decrement
+
+
+//example...
+let number1 = 10;
+let number2 = 2;
+
+console.log(number1 + number2); // 2ti valu jog kora
+console.log(number1 - number2); // 2ti valu biog kora
+console.log(number1 * number2); // 2ti valu gun kora
+console.log(number1 / number2); // 2ti valu vag kora
+console.log(number1 % number2); // 2ti valu vagses kora
+
+console.log(number1 ** number2); // 2ti valu 2squre kora
