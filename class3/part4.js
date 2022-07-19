e9d5891bb0a763b4c1ef45c65e5b3090bba658ea
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-19 22:43:41 +0600

    JavaScript Logical Operators
     JavaScript Logical Operators

---
+++ class3/part4.js
@@ -0,0 +1,23 @@
+/*
+//  JavaScript Logical Operators
+//  Operator	Description
+
+//  &&	    logical      and
+//  ||  	logical      or
+//  !	    logical      not
+*/
+
+//exampler
+
+let price =10;
+//logical oprator and usage
+console.log(price > 5 && price < 15); // print true 
+console.log(price > 50 && price < 15); // print false 
+
+//logical oprator or usage
+console.log(price > 50 || price < 15); // one expression true hole ei true return korbe
+
+//logical oprator not usage
+console.log(price > 5);// retuen true
+console.log(!(price > 5));// retuen false usage not oprator
+
