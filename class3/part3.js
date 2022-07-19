cfb1204ed5a0663e1ea7c322aa89b35af518f6f9
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-19 22:42:57 +0600

    JavaScript Comparison Operators
    JavaScript Comparison Operators

---
+++ class3/part3.js
@@ -0,0 +1,24 @@
+/*
+//  JavaScript Comparison Operators
+//  Operator	Description
+//  ==	equal to
+//  ===	equal value and equal type
+//  !=	not equal
+//  !==	not equal value or not equal type
+//  >	greater than
+//  <	less than
+//  >=	greater than or equal to
+//  <=	less than or equal to
+//  ?	ternary operator
+let price = 100;
+*/
+
+let price = 200;
+// greater than
+console.log( price > 200);//price valu theke boro kina 
+console.log( price >= 100); //greater than or equal to
+console.log( price < 50); //  <	less than
+console.log( price <= 500);//  <=	less than or equal to
+console.log( price === 200);//  ===	equal value and equal type
+console.log( price == 200);// //  ==	equal to
+console.log( price !== 200); //  !==	not equal value or not equal type
