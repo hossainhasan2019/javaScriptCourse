83ba97411927c5c8f412c64eef23348648f94d30
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-29 22:36:15 +0600

    For Loop
    for loop syntex 
    for(){
    
      }

---
+++ class5/part1.js
@@ -0,0 +1,18 @@
+//For Loop 
+
+/*  
+//for loop syntex 
+for(){
+
+  }
+
+*/
+//incriment for loop 
+for(let index = 0 ; index <= 10; index++){
+    console.log('Hello world' , index);
+}
+
+//dicriment for loop 
+for(let index = 10; index >= 0; index--){
+    console.log("hello World", index);
+}
