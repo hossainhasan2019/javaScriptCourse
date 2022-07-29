9c14465f4f70b6344c937c9e913966124a8eacc1
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-29 22:43:36 +0600

    nasted loop
    loop er vitor loop dile take nasted loop bole

---
+++ class5/part8.js
@@ -0,0 +1,24 @@
+// nasted loop 
+// loop er vitor loop dile take nasted loop bole 
+/*
+11
+12
+13
+14
+15
+
+
+21
+22
+23
+24
+25
+31
+*/
+for (let i = 1; i <= 5; i++){
+    //i = 1 2 3 4 5
+    for(let j = 1; j <= 5 ; j++){
+        //j = 1 2 3 4 5
+        console.log(i , j);
+    }
+}
