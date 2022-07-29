813669dc4523211ab94ad6a46c8f626c8eb31f41
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-29 22:42:50 +0600

    infinite loop

---
+++ class5/part7.js
@@ -0,0 +1,11 @@
+//infinite loop
+
+// always true hole infinite loop a pore jay
+
+//condition must be acurate dite hobe 
+
+//example 
+let index =1;
+while (true){
+    console.log("while loop ", index);
+}
