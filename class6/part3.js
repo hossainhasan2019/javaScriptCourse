0c705512227738a8ebb15d25938298e818fbeced
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-29 22:49:31 +0600

    search method string

---
+++ class6/part3.js
@@ -0,0 +1,19 @@
+let message = "Please repeat the 'repeat' Word";
+
+// search method string er first index valu find kore 
+// search() method er moddhe kono paramiter dia jayna 
+//regExp dia jay
+console.log(message.search('repeat'));
+
+//indexOf() method o same eki vabe string er first index er valo ene day 
+
+console.log(message.indexOf('repeat'));
+
+//indexof method a paramiter dia jay 
+console.log(message.indexOf('repeat',14));
+
+//lastIndexOf() method a index er ses theke suru hoy
+//and ei method a paramiter dia jay
+console.log(message.lastIndexOf('repeat',14));//0 - 13
+
+
