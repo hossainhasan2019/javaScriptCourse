705f94e7a099bc3f73ebb8095aa4ba5041bf4bd6
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-29 22:38:09 +0600

    do ... while loop
    do {
        // code block to be executed
      }
      while (condition);

---
+++ class5/part3.js
@@ -0,0 +1,24 @@
+// do ... while loop
+//syntex
+/*
+do {
+    // code block to be executed
+  }
+  while (condition);
+*/  
+//incriment do while loop
+//do while loop must 1bar cholbei
+let index = 0;
+do{
+    console.log('hello wold', index);
+    index++;
+}while(index <= 10 );
+
+
+//dicriment do while loop
+
+let index2 = 10;
+do{
+    console.log('hello wold', index2);
+    index2--;
+}while(index2 >= 0 );
