cff6ef0d2b81a6ad752c4e41058c877911ff67ee
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-29 22:28:41 +0600

    switch ... case

---
+++ class4/part2.js
@@ -0,0 +1,17 @@
+// switch ... case
+
+let color = 'green';
+
+switch(color){
+    case 'red':
+        console.log('this is red');
+        break;
+    case 'white':
+        console.log('this is white');  
+        break;
+    case 'green':
+         console.log('this is green');      
+         break;
+    default:
+        console.log('This is not a valid color');
+}
