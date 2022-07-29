af1fdd98f6eb33a4cb686b59a3fce2765f00f288
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-29 22:39:48 +0600

    for .... of loop
    for(let index of Array){
        console.log(index);
    }

---
+++ class5/part5.js
@@ -0,0 +1,16 @@
+// for .... of loop 
+// for of loop usage array
+//syntex
+for(let index of Array){
+    console.log(index);
+}
+
+
+//direct valu print korar jonno amra for of loop usage korte pari 
+
+const myArray = ['hossain', 'hasan', '45',454];
+
+for(let index of myArray){
+    console.log(index);
+}
+
