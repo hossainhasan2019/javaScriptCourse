e061b811bdcfc6b6c1b81364324d9f533847c4bc
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-29 22:27:57 +0600

    Conditional Statements

---
+++ class4/part1.js
@@ -0,0 +1,21 @@
+//Conditional Statements
+//
+//if(condition1){
+//    //statements
+//}else if (condition2){
+//    //statements
+//}else if (condition3){
+//    //statements
+//}else{
+//    //statements
+//}
+
+let number = -10;
+
+if(number > 0){
+    console.log('this is a prsitive number');
+}else if(number < 0){
+    console.log('This is a negative number');
+}else{
+    console.log('this is a zero')
+}
