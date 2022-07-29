4ac4d0b0add1fadf6b9f5ce6f49d6577ef481b07
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-29 22:52:17 +0600

    java script er kisu syntex \ /n /t

---
+++ class6/part6.js
@@ -0,0 +1,35 @@
+/*
+he said , "It' a sunny day" .
+have a good day.
+
+java script er kisu syntex
+1 - \' print cotation
+2 - \n line break 
+3 - \t create a tab somo poriman space
+4 - \/ slash print korar jonno 
+slash dia porer ta print korar jonno spatial charecter 
+*/
+// strin er moddhe backslash dia skip kora hoy and cotatioon parin kora jay
+
+let message = "he said it\'s a summyu day"
+console.log(message);
+
+// \n dia new line diclier kora hoy 
+let message2 = "he said it\'s a summyu day \n hello boys now to go"
+console.log(message2);
+
+
+// \t dia tab somo poriman space dia hoy
+let message3 = "he \t said it\'s a summyu day"
+console.log(message3);
+
+// back slash dia porer slash print kore eita java script er kisu syntex
+
+
+let message4 = "he\/\/ said it\'s a summyu day"
+console.log(message4);
+
+//example google link
+let googleLink = 'http:\/\/www.google.com'
+
+console.log(googleLink)
