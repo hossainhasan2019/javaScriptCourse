47ae5ef4190f1629f0f3d36bb256745faa796610
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-29 22:52:54 +0600

    string Immutability

---
+++ class6/part7.js
@@ -0,0 +1,8 @@
+// string Immutability
+
+
+// string er moddhe kono data assign korle tar moddhe poriborton kora jay na
+// string re assign kore change kora hoy
+// string toiri kora hole ta ke r update modyfi kora jayna take pori borton korte hole reassign korte hoy
+let message = " jello hossain"
+message[0] = 'h';
