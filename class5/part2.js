cda4fd80f9d103f01552cee27fde099c70f7afc5
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-29 22:37:19 +0600

    while loop
    while (condition) {
        // code block to be executed
      }

---
+++ class5/part2.js
@@ -0,0 +1,24 @@
+//whilw loop 
+// incriment while loop
+//syntex
+while (condition) {
+    // code block to be executed
+  }
+
+
+//example
+let index = 0 ;
+
+while( index <= 10){
+    console.log("hello World", index);
+    index++
+}
+
+//dicriment while loop 
+
+let index2 = 10 ;
+
+while( index2 >= 0){
+    console.log("hello World", index2);
+    index2--
+}
