a983d68f5af00ec6cf09a70fdbbc151f3887255e
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-07-29 22:47:22 +0600

    string basics

---
+++ class6/part1.js
@@ -0,0 +1,25 @@
+//string basics
+// stromg character traverse
+
+let country = 'bangladesh;'
+//print string
+console.log(country);
+
+//data type print usage typeof kyeword
+console.log(typeof country);
+
+//travarse character index wise
+console.log(country[3]);
+
+// jodi squre brackets dia index er valu besi dia hoy tahole print undefind
+console.log(country[12]);
+
+//print index er valu another value usage charAt() method
+console.log(country.charAt(4));
+
+//jodi charAt method usage kore index er valu besi dia hoy thaole print emty string
+console.log(country.charAt(14));
+
+//string er lenght ber korar jonno usage kori length method
+console.log(country.length);
+
