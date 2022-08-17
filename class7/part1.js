2c714900821aa3e53714152af8d50adcd8fb3f91
Hossain Hasan <96678579+hossainhasan2019@users.noreply.github.com>
2022-08-17 22:33:25 +0600

    javascript Object
    object declare

---
+++ class7/part1.js
@@ -0,0 +1,25 @@
+// javascript Object 
+// object declare korar somoy const usage korbo 
+
+//object declare korle object er 2ja jinis "proparti or valu thake"
+const user = {
+    name : "john",
+    age : 47 ,
+    hobby : 'rea'
+}
+console.log(user);
+
+//Dot notation usage
+// proparti er access nia
+console.log(user.age);
+console.log(user.name);
+
+//bracket notation
+console.log(user['name']);
+console.log(user['hobby']);
+console.log(user['age']);
+
+
+//end
+
+
